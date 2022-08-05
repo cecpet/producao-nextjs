aa706da6408d2c63f9daaafba86ae09a550844b7
Cec Pet <propaganda@cecpet.com>
2022-08-05 08:38:48 -0300

    Deploy on vercel

---
+++ components/Searchbar.js
@@ -0,0 +1,155 @@
+import { useState, useEffect } from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import { Button } from '@mui/material';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { Box } from '@mui/material/';
+import moment from 'moment';
+import axios from 'axios';
+
+
+export default function SearchBar(props) {
+    let nomeProdutos = [];
+    let codigoProdutos = [];
+    let estoqueProdutos = [];
+    const [produtos, setProdutos] = useState(null);
+    const [quantidade, setQuantidade] = useState('');
+    const [inputValue, setInputValue] = useState('');
+    const [data, setData] = useState(null);
+    const [maquina, setMaquina] = useState('');
+    const [codigo, setCodigo] = useState('');
+    const [estoque, setEstoque] = useState('');
+    const [id, setId] = useState(0);
+    const [rows, setRows] = useState(() => {
+        if (typeof window !== "undefined") {
+            let rowsStorage = localStorage.getItem('rows');
+            const initialRows = JSON.parse(rowsStorage) || [];
+            if (rowsStorage) {
+                return [initialRows];
+            };
+        }
+    });
+    
+
+    https://bling.com.br/Api/v2/produtos/json&estoque=S&apikey=${process.env.NEXT_PUBLIC_BLING_KEY}
+
+    useEffect(() => {
+        axios({
+            method: 'get',
+            mode: 'cors',
+            url: `https://bling.com.br/Api/v2/produtos/json&estoque=S&apikey=${process.env.NEXT_PUBLIC_BLING_KEY}`,
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            }
+        }).then(res => {setProdutos(res.data.retorno.produtos)})
+    }, [])
+
+
+    if(produtos !== null) {
+        produtos.forEach(item => {
+            let produtoNome = item.produto.descricao;
+            let produtoCodigo = item.produto.codigo;
+            let produtoEstoque = item.produto.estoqueAtual;
+            nomeProdutos.push(produtoNome);
+            codigoProdutos.push(produtoCodigo);
+            estoqueProdutos.push(produtoEstoque);
+        });
+    }
+
+    function handleData(newData){
+        setData(newData);
+    };
+
+    let produtoProduzido = {};
+
+    function handleQuantidade(event){
+        setQuantidade(event.target.value);
+    }
+
+    function handleAdd(){
+        if(inputValue === '' || data == null || quantidade === '' || maquina === ''){
+            alert('Preencha todos os campos');
+        } else {
+
+            console.log(localStorage.getItem('rows'));
+            setId(id=> id + 1);
+            produtoProduzido = {
+                produto: inputValue,
+                quantidade: quantidade,
+                data: moment(data).format('YYYY-MM-DD'),
+                maquina: maquina,
+                id: id,
+                codigo: codigo,
+                estoque: estoque,
+             }
+            props.addProduto(produtoProduzido);
+            console.log(estoqueProdutos)
+            setInputValue('');
+            setQuantidade('');
+            setData(null);
+            setMaquina('');
+        }
+    }
+
+  return (
+    <Box component="form" sx={{
+        display: 'flex',
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '1rem',
+    }}>
+        <Autocomplete
+            disablePortal
+            aria-required="true"
+            id="combo-box-demo"
+            options={nomeProdutos} 
+            sx={{ width: 300 }}
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+                setInputValue(newInputValue);
+                setCodigo(() => codigoProdutos[nomeProdutos.indexOf(newInputValue)]);
+                setEstoque(() => estoqueProdutos[nomeProdutos.indexOf(newInputValue)]);
+            }}
+            renderInput={(params) => <TextField {...params} label="Produtos" />}
+        />
+        <TextField id="outlined-basic" type="number" label="Quantidade" variant="outlined" value={quantidade} onChange={handleQuantidade}/>
+        <LocalizationProvider dateAdapter ={AdapterDateFns}>
+        <DatePicker
+          required  
+          label="Data"
+          inputFormat="dd/MM/yyyy"
+          value={data}
+          onChange={handleData}
+          renderInput={(params) => <TextField {...params} />}
+        />
+        </LocalizationProvider>
+        <FormControl>
+    <FormLabel id="demo-radio-buttons-group-label">Máquina</FormLabel>
+    <RadioGroup
+        aria-labelledby="demo-radio-buttons-group-label"
+        aria-required="true"
+        name="radio-buttons-group"
+        value={maquina}
+        onChange={(event, newMaquina) => {
+            setMaquina(newMaquina);
+        }}
+    >
+        <FormControlLabel value="MG120" control={<Radio />} label="MG120" />
+        <FormControlLabel value="MG200" control={<Radio />} label="MG200" />
+    </RadioGroup>
+    </FormControl>
+    <Button variant="contained" onClick={handleAdd}>Adicionar</Button>
+    </Box>
+    
+  );
+}
+
---
+++ pages/index.js
@@ -0,0 +1,144 @@
+import Head from 'next/head'
+import SearchBar from "../components/Searchbar";
+import { supabase } from "./supabaseClient";
+import  { useEffect, useState } from "react";
+import { DataGrid } from '@mui/x-data-grid';
+import { Box } from "@mui/material";
+import Button from "@mui/material/Button";
+import { toXML } from 'jstoxml';
+
+export default function Home() {
+  
+  const [produto, setProduto] = useState([]);
+  const [rowsDeletadas, setRowsDeletadas] = useState([]);
+
+
+  const addProduto = (item) => {
+    setProduto([...produto, item]);
+  }
+
+  const columns = [
+    {field: 'id', headerName: 'ID', width: 100},
+    {field: 'produto', headerName: 'Produto', width: 300, editable: true},
+    {field: 'quantidade', headerName: 'Quantidade', width: 120, editable: true},
+    {field: 'data', headerName: 'Data', width: 200, editable: true},
+    {field: 'maquina', headerName: 'Maquina', width: 150, editable: true},
+  ];
+
+
+  const rows = [...produto];
+
+
+  useEffect(() => {
+    if(produto.length > 0){
+      localStorage.setItem('rows', JSON.stringify(rows))
+    }
+  }, [produto])
+
+  useEffect(() => {
+    const rows = JSON.parse(localStorage.getItem('rows'));
+    if(rows) {
+      setProduto(rows);
+    }
+  }, [])
+
+
+  function enviarProdutos(){
+    rows.forEach(item => {
+
+      let qntd = parseInt(item.quantidade);
+      let estoque = parseInt(item.estoque);
+      let estoqueAtual = estoque + qntd;
+
+      let produtoXML= {
+        produto: {
+          codigo: item.codigo,
+          descricao: item.produto,
+          estoque: estoqueAtual,
+      }
+    }
+
+      
+      let xml = toXML(produtoXML);
+      console.log(xml);
+
+      fetch(`https://bling.com.br/Api/v2/produto/${item.codigo}/json&estoque=S&apikey=bdb022ba4d05d05a2b152b04843dc465ef6df0a543ddb2152fc87379aa37d7e697cd62c6&xml=${xml}`, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+      })
+      .then(response => response.json())
+      .catch(error => console.log(error));
+      
+      async function insertProdutos(){
+        const { data, error } = await supabase
+        .from('produtos')
+        .insert(
+          {
+            produto: item.produto,
+            qntd: item.quantidade,
+            data: item.data,
+            maquina: item.maquina,
+          },
+          { returning: 'minimal' }
+        )
+        console.log(error, data)
+  
+       }
+        insertProdutos();
+    })
+    
+    localStorage.clear();
+    window.alert('Produtos enviados com sucesso!');
+
+
+
+  }
+
+  const handleRowsSelected = (rows) => {
+    
+  }
+  return (
+    <div>
+      <Head>
+        <title>Produção - Cec Pet</title>
+        <meta name="description" content="Produção Cec Pet" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Cec Pet - Produção</h1>
+      <SearchBar addProduto={addProduto} />
+      <Box sx={{
+        height: 350,
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '1rem',
+      }}>
+        <DataGrid 
+        columns={columns} 
+        rows={produto}
+        experimentalFeatures={{ newEditingApi: true }}
+        checkboxSelection={true}
+        rowSelection={true}
+        rowSelectionMode={'multiple'}
+        onRowSelected={handleRowsSelected(rows)}
+        />
+      </Box>
+      <Box sx={{
+        textAlign: 'right',
+        padding: '.5rem',
+        display: 'flex',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        gap: '1rem',
+      }}>
+        <Button variant="contained">Deletar Selecionados</Button>
+        <Button onClick={enviarProdutos} variant="contained">Enviar</Button>
+      </Box>
+     
+    </div>
+  )
+}
